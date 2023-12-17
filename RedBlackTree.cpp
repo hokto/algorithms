56ce33731ac5db281ed0998135d932c179338080
hokto <hokuto.k1441@gmail.com>
2023-12-17 18:24:26 +0900

    pair型で動作することを確認した

--- RedBlackTree.cpp
+++ RedBlackTree.cpp
@@ -384,7 +384,15 @@ template<typename U> class RedBlackTree{
                 return 0;
             }
         }
-
+        // 赤黒木が空かどうか
+        int empty(){
+            if(root == NIL){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
 };
 
 int main(){
@@ -428,6 +436,7 @@ int main(){
     return 0;
     */
     // verified(https://algo-method.com/tasks/442)
+    /*
     int N;
     cin>>N;
     vector<int> A(N);
@@ -435,5 +444,32 @@ int main(){
     RedBlackTree<int> rb(A);
     for(int v : rb.sort()) cout<<v<<" ";
     cout<<endl;
-    
+    return 0;
+    */
+   // verified(https://atcoder.jp/contests/abc137/tasks/abc137_d) 
+   int N,M;
+   cin>>N>>M;
+   RedBlackTree<pair<long long,int>> rb;
+   vector<vector<long long>> D(M+1);
+   for(int i=0;i<N;i++){
+    int A;
+    long long B;
+    cin>>A>>B;
+    if(A>M) continue;
+    D[A].push_back(B);
+   }
+    long long ans = 0;
+    for(int A=1;A<=M;A++){
+        for(long long B : D[A]){
+            Node<pair<long long,int>>* node = new Node<pair<long long,int>>;
+            node->key = {-B,A};
+            rb.insert(node);
+        }
+        if(!rb.empty()){
+            Node<pair<long long,int>>* min = rb.minimize(rb.get_root());
+            ans += -1*min->key.first;
+            rb.delete_at(min);
+        }
+    }
+    cout<<ans<<endl;
 }
